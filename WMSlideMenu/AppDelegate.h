c42d479d9b8720d1168913cc4dd8812ac4bb912e
maginawin <maginawin@163.com>
2015-03-06 16:25:47 +0800

    Initial Commit

---
+++ WMSlideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMSlideMenu
+//
+//  Created by maginawin on 15-3-6.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
